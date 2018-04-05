ef2e13c527334b1835935645b9daa07bdc3cb5ed
Santo Commarato <cylynex@gmail.com>
2018-04-05 09:11:23 -0600

    Node hover states

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+    [Header("Attributes")]
+    public Color hoverColor;
+
+    private Renderer rend;
+    private Color startColor;
+
+    void Start() {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+	// hover animation
+    void OnMouseEnter() {
+        rend.material.color = hoverColor;
+    }
+
+    void OnMouseExit() {
+        rend.material.color = startColor;
+    }
+}
