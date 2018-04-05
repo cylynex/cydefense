748d39f2562560e117d2432aeebd52465c34175f
Santo Commarato <cylynex@gmail.com>
2018-04-05 13:53:13 -0600

    camera changes 2

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,39 +6,65 @@ public class CameraController : MonoBehaviour {
 
     [Header("Attributes")]
     public float panSpeed = 30f;
+    public float panBorderThickness = 10f;
+    public float scrollSpeed = 5f;
+    public float minY = 10f;
+    public float maxY = 80f;
 
-    // The world-position of the mouse last frame.
-    Vector3 lastFramePosition;
-    Vector3 currFramePosition;
+    [Header("Camerea")]
+    public Vector2 zoomRange = new Vector2(-50, 100);
+    public float CurrentZoom = 0;
+    public float ZoomSpeed = 1;
+    public float ZoomRotation = 1;
+    public Vector2 zoomAngleRange = new Vector2(20, 70);
+    public float rotateSpeed = 10;
+
+    private Vector3 initialPosition;
+    private Vector3 initialRotation;
+
 
     void Update() {
-        /*
-        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - 10) {
+
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height * panBorderThickness) {
+            Debug.Log("up");
             transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
         }
-        */
-
-        UpdateCameraMovement();
-
-        // Save the mouse position from this frame
-        lastFramePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        lastFramePosition.z = 0;
-
-    }
-
-
-    void UpdateCameraMovement() {
-        // movement
-        if (Input.GetMouseButton(1)) {   // Right Mouse Button
-
-            Vector3 diff = lastFramePosition - currFramePosition;
-            Camera.main.transform.Translate(diff);
-
+            if (Input.GetKey("s") || Input.mousePosition.y >= Screen.height * panBorderThickness) {
+            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+        }
+            if (Input.GetKey("a") || Input.mousePosition.x >= Screen.width * panBorderThickness) {
+            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+        }
+            if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width * panBorderThickness) {
+            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
         }
 
-        Camera.main.orthographicSize -= Camera.main.orthographicSize * Input.GetAxis("Mouse ScrollWheel");
+        // Mouse scroll zoom
+        /*
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        Debug.Log(scroll); 
+        Vector3 position = transform.position;
+        position.y -= scroll * 1000 * scrollSpeed * Time.deltaTime;
 
-        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, 3f, 25f);
+        // restrict Y
+        position.y = Mathf.Clamp(position.y, minY, maxY);
+        transform.position = position;
+        */
+
+
+        // zoom in/out
+        CurrentZoom -= Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime * 1000 * ZoomSpeed;
+        CurrentZoom = Mathf.Clamp(CurrentZoom, zoomRange.x, zoomRange.y);
+        transform.position = new Vector3(transform.position.x, transform.position.y - (transform.position.y - (initialPosition.y + CurrentZoom)) * 0.1f, transform.position.z);
+
+        float x = transform.eulerAngles.x - (transform.eulerAngles.x - (initialRotation.x + CurrentZoom * ZoomRotation)) * 0.1f;
+        x = Mathf.Clamp(x, zoomAngleRange.x, zoomAngleRange.y);
+
+        transform.eulerAngles = new Vector3(x, transform.eulerAngles.y, transform.eulerAngles.z);
+
     }
+
+
+
 
 }
