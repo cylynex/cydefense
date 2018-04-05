7774b09f16e4f0c619b6ae5a889a7815a8cf8e59
Santo Commarato <cylynex@gmail.com>
2018-04-05 09:02:07 -0600

    Drone model animations and turning

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,18 +4,28 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
-    public float speed = 25f;
+    [Header("Attributes")]
+    public float speed = 5f;
+    public float turnSpeed = 25f;
 
     private Transform target;
     private int wayPointIndex = 0;
+    private Utility utility;
 
 	// Use this for initialization
 	void Start () {
         target = Waypoints.points[0];
+        //utility = new Utility();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        // Turn
+        Vector3 directonToFace = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(directonToFace);
+        Vector3 rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        transform.rotation = Quaternion.Euler(0f, rotation.y, 0f);
 
         // Pick direction and move
         Vector3 direction = target.transform.position - transform.position;
@@ -37,6 +47,7 @@ public class Enemy : MonoBehaviour {
             // go to next WP
             wayPointIndex++;
             target = Waypoints.points[wayPointIndex];
+
         }
     }
 }
---
+++ Assets/Scripts/Utility.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utility : MonoBehaviour {
+
+	// face target
+    public void FaceTarget(Transform target, float turnSpeed) {
+        /*
+        Vector3 directonToFace = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(directonToFace);
+        Vector3 rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        transform.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+        return transform.rotation;
+        */
+    }
+}
