136c6d7321c4e1cbeae4847912b3a65c621a3f92
Santo Commarato <cylynex@gmail.com>
2018-04-19 10:01:21 -0600

    Added Fader script for level changes and prefab

---
+++ Assets/Scripts/Fader.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Fader : MonoBehaviour {
+    
+    public Image image;
+    public float speedValue = 0.5f;
+    public AnimationCurve animCurve;
+
+    void Start() {
+        StartCoroutine(FadeIn());
+    }
+
+
+    public void FadeTo(string scene) {
+        StartCoroutine(FadeOut(scene));
+    }
+                               
+    IEnumerator FadeIn() {
+        float alphaVal = 1f;
+
+        while (alphaVal > 0f) {
+            alphaVal -= Time.deltaTime * speedValue;
+            float curveVal = animCurve.Evaluate(alphaVal);
+            image.color = new Color(0f, 0f, 0f, alphaVal);
+            yield return 0;
+        }
+    }
+
+    IEnumerator FadeOut(string scene) {
+        float alphaVal = 0f;
+
+        while (alphaVal < 1f) {
+            alphaVal += Time.deltaTime * speedValue;
+            float curveVal = animCurve.Evaluate(alphaVal);
+            image.color = new Color(0f, 0f, 0f, alphaVal);
+            yield return 0;
+        }
+
+        // Load the scene now
+        SceneManager.LoadScene(scene);
+    }
+
+}
